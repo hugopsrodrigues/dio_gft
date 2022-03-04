fe3c3a9c90e29d6dc2767338429d03724e719aa9
Hugo Rodrigues <hugopsr@hotmail.com>
2022-03-04 16:46:28 -0300

    Nova Interface

--- src/main/java/CalculadoraEEmprestimo/Calculadora.java
+++ src/main/java/CalculadoraEEmprestimo/Calculadora.java
@@ -1,19 +1,20 @@
 package CalculadoraEEmprestimo;
 
-public class Calculadora {
+public class Calculadora implements OperacaoMatematica{
 
-    public Double Soma (double a, double b){
+    @Override
+    public Double Soma(double a, double b){
         return a + b;
     }
-
+    @Override
     public Double Sub (double a, double b){
         return a - b;
     }
-
+    @Override
     public Double Multi (double a, double b){
         return a * b;
     }
-
+    @Override
     public Double Div (double a, double b){
         return a / b;
     }
---
+++ src/main/java/CalculadoraEEmprestimo/OperacaoMatematica.java
@@ -0,0 +1,11 @@
+package CalculadoraEEmprestimo;
+
+public interface OperacaoMatematica {
+     Double Soma(double operando1, double operando2);
+
+     Double Sub(double operando1, double operando2);
+
+    Double Multi(double operando1, double operando2);
+
+    Double Div(double operando1, double operando2);
+}
